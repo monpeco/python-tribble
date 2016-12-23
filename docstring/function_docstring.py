2e07b154c70cb05fd8192a662ee54b7dfe652048
monpeco <monpeco@gmail.com>
2016-12-23 14:40:43 +0000

    Use of docstring

---
+++ docstring/function_docstring.py
@@ -0,0 +1,10 @@
+"""
+Assuming this is file mymodule.py, then this string, being the
+first statement in the file, will become the "mymodule" module's
+docstring when the file is imported.
+"""
+
+def printme( str ):
+   "This prints a passed string into this function"
+   print (str)
+   return
