8b2384b25f5100d692f6c8a476f929dccc576e76
monpeco <monpeco@gmail.com>
2017-02-14 11:59:36 +0000

    Change fully object to use SumableList construtor to initialize var

--- inde_full_object.py
+++ inde_full_object.py
@@ -6,11 +6,9 @@ class SummableList(list):
             print("v: %s" % v)
         return s
         
-m = list()
+m2 = SummableList()
 for n in range(1, 10):
     if n % 3 == 0 or n % 5 == 0:
-        m.append(n)
-
-k = SummableList(m)
-print(k.suma())
-
+        m2.append(n)
+        
+print(m2.suma())
